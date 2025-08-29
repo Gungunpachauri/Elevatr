6a9cfd96f760f636d737e20f220c9f43294ebf8a
Your Name <gungunpachauri13@gmail.com>
2025-08-29 23:34:59 +0530

    Author Authentication

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,21 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  images:{
+     dangerouslyAllowSVG: true,
+    remotePatterns:[
+    {
+      protocol: 'https',
+      hostname: '*',
+    }
+    ]
+  },
+  experimental:{
+    ppr: 'incremental',
+  },
+  devIndicators:{
+    position: "bottom-right",
+  },
   /* config options here */
 };
 
